3822afa2aeba18e30f4d5884b9a08deda5523644
w0www <imanol@alvarezperez.net>
2012-10-30 10:15:41 +0100

    mejoras en el diseño

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,10 @@ class CategoriesController < ApplicationController
   hobo_model_controller
 
   auto_actions :all
+  
 #  auto_actions_for :expense, :all
 #  auto_actions_for :income, :all
+
+
 
 end
--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,6 +5,27 @@
 Beneficios::Application.routes.draw do
 
 
+  # Resource routes for controller categories
+  resources :categories
+
+
+  # Resource routes for controller expenses
+  resources :expenses
+
+  # Owner routes for controller expenses
+  resources :categories, :as => :category, :only => [] do
+    resources :expenses, :only => [] do
+      collection do
+        post 'create', :action => 'create_for_category'
+      end
+    end
+  end
+
+
+  # Resource routes for controller incomes
+  resources :incomes
+
+
   # Resource routes for controller users
   resources :users, :only => [:edit, :show, :create, :update, :destroy] do
     collection do
@@ -22,26 +43,5 @@ Beneficios::Application.routes.draw do
   match 'login(.:format)' => 'users#login', :as => 'user_login'
   get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
-
-
-  # Resource routes for controller incomes
-  resources :incomes
-
-
-  # Resource routes for controller expenses
-  resources :expenses
-
-  # Owner routes for controller expenses
-  resources :categories, :as => :category, :only => [] do
-    resources :expenses, :only => [] do
-      collection do
-        post 'create', :action => 'create_for_category'
-      end
-    end
-  end
-
-
-  # Resource routes for controller categories
-  resources :categories
 
 end
